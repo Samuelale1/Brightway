d4a5c8cd8e8f4f6da52259cadd1929a351f2abdd
Samuelale1 <alesamuelayomide@gmail.com>
2026-01-16 14:03:50 +0100

    fixed the missing import

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\SalespersonController;
 use App\Http\Controllers\OrderController;
